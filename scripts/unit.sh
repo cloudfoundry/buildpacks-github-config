f26f91f3150852047432646f5f84bf0c06d848f9
Arjun Sreedharan <asreedharan@vmware.com>
2023-01-31 21:56:00 +0000

    run unit tests on PR

---
+++ scripts/unit.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+readonly ROOT_DIR="$(cd "$(dirname "${0}")/.." && pwd)"
+
+# shellcheck source=SCRIPTDIR/.util/print.sh
+source "${ROOT_DIR}/scripts/.util/print.sh"
+
+function main() {
+  while [[ "${#}" != 0 ]]; do
+    case "${1}" in
+      --help|-h)
+        shift 1
+        usage
+        exit 0
+        ;;
+
+      "")
+        # skip if the argument is empty
+        shift 1
+        ;;
+
+      *)
+        util::print::error "unknown argument \"${1}\""
+    esac
+  done
+
+  while read -r directory; do
+    pushd "${directory}" > /dev/null || return
+      go test -v -count=1 .
+    popd > /dev/null || return
+  done < <(find "${ROOT_DIR}" -type d -name entrypoint ! -path '*/.git/*')
+}
+
+function usage() {
+  cat <<-USAGE
+unit.sh [OPTIONS]
+
+Runs the unit test suite.
+
+OPTIONS
+  --help  -h  prints the command usage
+USAGE
+}
+
+main "${@:-}"
